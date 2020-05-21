968880c1195cdb54edb102aeb6e31476b60db800
Luca <duvet86@gmail.com>
2020-05-21 21:30:45 +0800

    Initial Commit.

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.post("/", function (req, res, next) {
+  const { username, password } = req.body;
+  if (username !== "test" || password !== "test") {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  res.json(req.body);
+});
+
+module.exports = router;
